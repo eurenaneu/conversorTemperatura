34f2480015a2245ec69bb3630797ea80849cab00
Renan Machado <renanpando@gmail.com>
2023-03-24 03:05:16 -0300

    fix: add error message and check for null

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let temp, tempConvertida, prefix;
+const resultado = document.getElementById("resultText");
 
 document.getElementById("submitButton").onclick = function(){
 
@@ -6,22 +7,20 @@ document.getElementById("submitButton").onclick = function(){
 
     temp = Number(document.getElementById("tempField").value);
 
-    if(document.getElementById("celsiusOption").checked){
-        prefix = "ºC";
-        tempConvertida = Math.round(((temp - 32)/1.8)*10)/10; // temperatura em celsius (formatado como X.XXºC)
-    } 
-    
-    else if(document.getElementById("fahrenheitOption").checked){
-        prefix = "ºF";
-        tempConvertida = Math.round((temp*1.8 + 32)*10)/10; // temperatura em fahrenheit (formatado como X.XXºC)
-    }
+    if(!isNaN(temp) && !temp == false){
+        if(document.getElementById("celsiusOption").checked){
+            prefix = "ºC";
+            tempConvertida = Math.round(((temp - 32)/1.8)*10)/10; // temperatura em celsius (formatado como X.XXºC)
+        } 
+        
+        else if(document.getElementById("fahrenheitOption").checked){
+            prefix = "ºF";
+            tempConvertida = Math.round((temp*1.8 + 32)*10)/10; // temperatura em fahrenheit (formatado como X.XXºC)
+        }
 
-    if (tempConvertida == undefined){
-        window.alert("Entrada inválida");
-    }
-    
-    else { 
-        document.getElementById("resultText").innerHTML = "<strong>RESULTADO:</strong> " + tempConvertida + prefix;
+        resultado.innerHTML = `<strong>RESULTADO:</strong> ${tempConvertida} ${prefix}`;
+    } else {
+        resultado.innerHTML = "\u26A0 <strong>ENTRADA INVÁLIDA</strong> \u26A0";
     }
 
 }
